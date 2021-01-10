45ac4e837c005a48aac0c95ed9d788b14b56e41e
Doug Sloan <drxsloan@gmail.com>
2021-01-10 20:01:31 +0000

    First commit - 1 panel, whole app not quite working

---
+++ week_05/weekend_homework/weekend_homework_shiny/app.R
@@ -0,0 +1,93 @@
+library(shiny)
+library(tidyverse)
+library(CodeClanData)
+
+game_data <- CodeClanData::game_sales
+
+ui <- fluidPage(
+    
+    theme = "cerulean",
+    
+    titlePanel("Games Data"),
+    
+    tabsetPanel(
+        tabPanel("Sales vs User Scores",
+                 fluidRow(
+                     column(4,
+                 
+                
+# So it would be good at this stage if we could work out how to get the select inputs to relate to one another. In other words, if "Nintendo" is selected for publisher, then only consoles that run Nintento's games should show up in the latter selectInput. Group by or maybe we can get it right in here??
+
+                selectInput("publisher_input",
+                            "Publisher",
+                           choices = unique(game_data$publisher)
+                )
+                    ),
+                 
+                    column(4,
+                
+                selectInput("platform_input",
+                            "Platform",
+                            choices = unique(game_data$platform)
+                )
+                    ),
+
+                    column(8,
+
+                sliderInput("year_input", 
+                            "Year of Release Range",
+                            min = min(unique(game_data$year_of_release)),
+                            max = max(unique(game_data$year_of_release)),
+                            value = as.numeric(c(1988, 2008))
+                )
+                    ),
+
+                    column(12, 
+                           
+                          actionButton("update", "Find Games")
+                    ),
+                    
+        fluidRow(
+            
+            column(8,
+
+        plotOutput("sales_user_output")
+        
+            )
+        )
+        )
+    )
+    
+
+
+    
+)
+
+)
+
+
+server <- function(input, output) {
+    
+    game_data_plus <- eventReactive(input$update, {
+
+        game_data %>%
+            filter(publisher == input$publisher_input) %>% 
+            filter(platform == input$platform_input) %>%
+            filter(year_of_release == input$year_input)
+             
+            
+    })
+    
+        output$sales_user_output <- renderPlot({
+            ggplot(game_data_plus()) +
+            aes(x = sales, y = user_score) +
+            geom_point() +
+            xlab("\nSales (millions)") +
+            ylab("User Rating\n")
+           
+    
+    })
+}
+
+
+shinyApp(ui = ui, server = server)
